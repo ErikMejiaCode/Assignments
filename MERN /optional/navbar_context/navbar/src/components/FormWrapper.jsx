72a7b58524db8d1ae57e41da3af5067771400414
Erik Mejia <eriknayarit352@gmail.com>
2022-11-02 16:43:20 -0400

    Navbar Context

---
+++ MERN /optional/navbar_context/navbar/src/components/FormWrapper.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Form from "./Form"
+
+
+
+const FormWrapper = () => {
+
+    return (
+        <>
+            <Form/>
+        </>
+    )
+}
+
+export default FormWrapper
