dcc0abe1fae4681ceeb0eae934c7b5305cd874c0
Erik Mejia <eriknayarit352@gmail.com>
2022-11-30 19:50:34 -0500

    C# -  Drink Maker Inheritance/Polymorphism/Methods

---
+++ CSharp_dotNET/practice/DrinkMaker/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Soda Coke = new Soda("Coca Cola", "brown", 38, 140, false);
+Coffee Cappuccino = new Coffee("French Vanilla Cappuccino", "light brown", 120, 110, "light", "coffee beans");
+Wine Merlot = new Wine("Merlot", "red", 32, 120, "Bordeaux", 1992);
+
+// Create a List of Drinks called ALlBeverages and add all instances to it
+List<Drink> AllBeverages = new List<Drink>();
+AllBeverages.Add(Coke);
+AllBeverages.Add(Cappuccino);
+AllBeverages.Add(Merlot);
+
+// Loop through your List of AllBeverages and call the ShowDrink() method for each
+foreach(Drink d in AllBeverages)
+{
+    d.ShowDrink();
+}
