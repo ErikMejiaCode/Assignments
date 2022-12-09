2e454ad802af47323ffad063f1c74bfc27b07565
Erik Mejia <eriknayarit352@gmail.com>
2022-12-09 00:39:23 -0500

    Login & Register - with Validations/Session

---
+++ CSharp_dotNET/core/LoginAndRegistration/Controllers/HomeController.cs
@@ -0,0 +1,130 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using LoginAndRegistration.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace LoginAndRegistration.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    private MyContext _context;
+
+    public HomeController(ILogger<HomeController> logger, MyContext context)
+    {
+        _logger = logger;
+        _context = context;
+    }
+
+
+    //Initial home page. Register and Login forms are held in this route.
+    [HttpGet("")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+
+    //Post route used to create new users upon registering successfully.
+    //passwords are hashed and session is set here as well
+    [HttpPost("users/create")]
+    public IActionResult CreateUser(User newUser)
+    {
+        if (ModelState.IsValid)
+        {
+            //Has our passwords | After validations are done 
+            PasswordHasher<User> Hasher = new PasswordHasher<User>();
+            newUser.Password = Hasher.HashPassword(newUser, newUser.Password);
+            _context.Add(newUser);
+            _context.SaveChanges();
+            HttpContext.Session.SetInt32("UserId", newUser.UserId);
+            return RedirectToAction("Success");
+        }
+        else
+        {
+            return View("Index");
+        }
+    }
+
+    //Once a user is registered successfully they are brought to this page.
+    //Once user logs in successfully they also land on this page.
+    [SessionCheck]
+    [HttpGet("success")]
+    public IActionResult Success()
+    {
+        return View();
+    }
+
+    [HttpPost("users/login")]
+    public IActionResult LoginUser(LogUser loginUser)
+    {
+        if (ModelState.IsValid)
+        {
+            //Look up our user in the database
+            User? userInDb = _context.Users.FirstOrDefault(u => u.Email == loginUser.LEmail);
+            //Verify user exists
+            if (userInDb == null)
+            {
+                ModelState.AddModelError("LEmail", "Invalid Email/Password");
+                return View("Index");
+            }
+            //Verify if the password match what is in the database
+            PasswordHasher<LogUser> hasher = new PasswordHasher<LogUser>();
+            var result = hasher.VerifyHashedPassword(loginUser, userInDb.Password, loginUser.LPassword);
+            if (result == 0)
+            {
+                //A failure, we did not use the right password 
+                ModelState.AddModelError("LEmail", "Invalid Email/Password");
+                return View("Index");
+            }
+            else
+            {
+                //set session and head to Success
+                HttpContext.Session.SetInt32("UserId", userInDb.UserId);
+                return RedirectToAction("Success");
+            }
+        }
+        else
+        {
+            return View("Index");
+        }
+    }
+
+    //Route used on a Button to clear session and re-route user back to index page 
+    [HttpPost("logout")]
+    public IActionResult Logout()
+    {
+        HttpContext.Session.Clear();
+        return RedirectToAction("Index");
+    }
+
+
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
+
+//Custom session validation is made. Can be added to pages where user sees information. 
+//On this project its only used on the Success page.
+public class SessionCheckAttribute : ActionFilterAttribute
+{
+    public override void OnActionExecuting(ActionExecutingContext context)
+    {
+        int? userId = context.HttpContext.Session.GetInt32("UserId");
+        if (userId == null)
+        {
+            context.Result = new RedirectToActionResult("Index", "Home", null);
+        }
+    }
+}
+
